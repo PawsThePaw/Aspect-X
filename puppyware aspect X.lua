98c32b761fb6596936a3990a3bed579e8a078f94
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-02-03 15:47:11 +0800

    Update and rename puppyware aspect X.lua.txt to puppyware aspect X.lua

--- puppyware aspect X.lua
+++ puppyware aspect X.lua
@@ -1205,3 +1205,16 @@ gm.__namecall = newcclosure( function(self, ...)
     end
 	return namecall(self, unpack(args))
 end)
+--//Auto reload
+_G.AutoReload = true -- change to false if u don't want it anymore.
+
+while _G.AutoReload == true and game:GetService("RunService").Heartbeat:Wait() do
+if game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Tool") then
+            if game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Tool"):FindFirstChild("Ammo") then
+                if game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Tool"):FindFirstChild("Ammo").Value <= 0 then
+                    game:GetService("ReplicatedStorage").MainEvent:FireServer("Reload", game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Tool")) 
+                    wait(1)
+                end
+            end
+        end
+end
